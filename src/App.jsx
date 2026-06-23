5024b385a388ccb30843e78c89451bab14eb5d65
PranavHarane <pranavharne00@gmail.com>
2026-06-23 19:44:37 +0530

    Changes in requested URL

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ const App = () => {
     }, []);
 
   return <div>
-    <div className="bg-[url('/public/BackgroundImage.jpg')] bg-cover h-[100vh] flex justify-center items-center">
-      <div id="weather-card" className="h-[56vh] w-[300px] sm:w-[400px] md:h-[500px] lg:w-[400px] text-center flex flex-col justify-between rounded-[20px] backdrop-blur-sm shadow-2xl p-[30px] bg-[rgba(255_255_255_0.5)]">
+    <div className="bg-[url('/BackgroundImage.jpg')] bg-cover h-[100vh] flex justify-center items-center">
+      <div className="h-[56vh] w-[300px] sm:w-[400px] md:h-[500px] lg:w-[400px] text-center flex flex-col justify-between rounded-[20px] backdrop-blur-sm shadow-2xl p-[30px] bg-[rgba(255_255_255_0.5)]">
         <InputBox function={getdata}/>
         <WeatherInfo Data={data}/>
       </div>
--- src/service/weatherService.jsx
+++ src/service/weatherService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const weatherService = async (cityname) =>{
-    const weatherData = await axios.get(`http://api.weatherapi.com/v1/current.json?key=10f3ae4d6a0e43d6ba3165013252907&q=${cityname}&aqi=yes`);
+    const weatherData = await axios.get(`https://api.weatherapi.com/v1/current.json?key=10f3ae4d6a0e43d6ba3165013252907&q=${cityname}&aqi=yes`);
     return weatherData.data;
 }
 
