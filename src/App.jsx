e33777875b6b5f5be2a8f3e2be31f6eb59f6a302
PranavHarane <pranavharne00@gmail.com>
2026-06-23 19:11:39 +0530

    Use react-toastify or flashmessages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import InputBox from './components/InputBox';
-import Error from './components/Error';
 import WeatherInfo from './components/WeatherInfo';
-import weatherService from './assets/weatherService'
+import weatherService from './service/weatherService'
+import {toast , ToastContainer , Bounce} from 'react-toastify'
 
 const App = () => {
   
   const [data , setData] = useState({});
   
-  let [errorMess , setErrorMess] = useState("");
-
   const getdata = async (cityname) => {
     try{
       const weatherData = await weatherService(cityname);
       setData(weatherData);
-    }catch(error){
-      if(error.response){
-        setErrorMess(error.response.data.error.message);
-      }else{
-        setErrorMess("Network Error");
+      }catch(error){
+        toast.error("Network Error!")
       }
-    }
     }
 
     useEffect(() => {
@@ -29,12 +23,24 @@ const App = () => {
 
   return <div>
     <div className="bg-[url('/public/BackgroundImage.jpg')] bg-cover h-[100vh] flex justify-center items-center">
-      {errorMess && <Error error={errorMess}/>}
       <div id="weather-card" className="h-[56vh] w-[300px] sm:w-[400px] md:h-[500px] lg:w-[400px] text-center flex flex-col justify-between rounded-[20px] backdrop-blur-sm shadow-2xl p-[30px] bg-[rgba(255_255_255_0.5)]">
         <InputBox function={getdata}/>
         <WeatherInfo Data={data}/>
       </div>
     </div>
+    <ToastContainer
+      position="top-center"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={false}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="light"
+      transition={Bounce}
+    />
   </div>
 }
 
